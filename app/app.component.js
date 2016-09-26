f2942187261edd36d46dd63cfc5423af3154a009
cyj <kelvin@mos.asia>
2016-09-26 21:07:37 +0800

    Step 1

--- app/app.component.js
+++ app/app.component.js
@@ -2,9 +2,23 @@
   app.AppComponent =
     ng.core.Component({
       selector: 'my-app',
-      template: '<h1>My First Angular App</h1>'
+      template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name"/>
+    </div>
+    `
     })
     .Class({
-      constructor: function() {}
+      constructor: function() {
+      	this.title = 'Tour of Heroes';
+  		this.hero = {
+  			id:1,
+  			name:'Windstorm'
+  		};
+      }
     });
 })(window.app || (window.app = {}));
